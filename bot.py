4ae2710bf222f6f2d2a4641aeffed77b9728f04e
james <jameshaller27@gmail.com>
2021-04-24 20:42:15 -0700

    Add info message w/ link to bot token

--- bot.py
+++ bot.py
@@ -21,5 +21,6 @@ token_env_name = 'DISCORD_TOKEN'
 bot_token = os.environ.get(token_env_name)
 if bot_token is None:
     print(f"'{token_env_name}' not found in environment. Failed to start bot.", file=sys.stderr)
+    print(f"Go to https://discord.com/developers/applications/790129414310068224/bot + authenticate to retrieve token")
     sys.exit(1)
 bot.run(bot_token)
